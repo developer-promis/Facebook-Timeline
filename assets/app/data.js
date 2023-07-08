e439b45c7cfcc4378080204e8adf760e2fc25647
PROMIS BASUNIA <126019991+developer-promis@users.noreply.github.com>
2023-07-08 17:41:49 +0600

    Add files via upload

---
+++ assets/app/data.js
@@ -0,0 +1,125 @@
+const post_list = [
+  {
+    id: 1,
+    post_publish_date: `5h`,
+    post_content: `
+    Thank you so much 'Donor'
+    for giving such a special moment of innovation!
+    May your Eid be celebrated with sacrifice & joy.<br>
+    #EidUlAdhaMubarak
+    `,
+      post_attachment: `./assets/images/post_image/rakib.jpg`,
+    likes: "83",
+    comments: "17",
+    author: {
+      name: "Rakibb ",
+      author_url: `https://web.facebook.com/rahatul.rakib.5`,
+      author_image:`./assets/images/author/rakib.jpg`,
+    },
+  },
+  {
+    id: 2,
+    post_publish_date: `21h`,
+    post_content: `
+  ভালোবাসার গল্প✌️✌️
+    `,
+    post_attachment: `./assets/images/post_image/meraj.jpg`,
+    likes: "45",
+    comments: "25",
+    author: {
+      name: "Mon Marajul",
+      author_url: `https://web.facebook.com/mdmarajul.islam.1806`,
+      author_image:`./assets/images/author/meraj.jpg`,
+
+    },
+  },
+  {
+    id: 3,
+    post_publish_date: `12h`,
+    post_content: `
+    স্ত্রীরের সাথে ঈদ উদযাপন করেছেন <br>
+    শামিম পাটোয়ারী 🥰🥰
+    `,
+    post_attachment: `./assets/images/post_image/wahid.jpg`,
+
+    likes: "100",
+    comments: "50",
+    author: {
+      name: "Limon",
+      author_url: `https://web.facebook.com/profile.php?id=100085706851382`,
+      author_image:`./assets/images/author/wahid.jpg`,
+
+    },
+  },
+  {
+    id: 4,
+    post_publish_date: `10h`,
+    post_content: `
+    এই দরজাটা যদি চিনে থাকো, তাহলে তোমার শৈশবকাল মধুময় ছিলো 🙂
+    `,
+    post_attachment: `./assets/images/post_image/naim.jpg`,
+
+    likes: "80",
+    comments: "45",
+    author: {
+      name: "Deowan Mohammad Naim",
+      author_url: `https://web.facebook.com/profile.php?id=100067990127179`,
+      author_image:`./assets/images/author/naim.jpg`,
+
+    },
+  },
+
+  {
+    id: 6,
+    post_publish_date: `May 21 at 5:23 PM`,
+    post_content: `
+    অবহেলায়, অনাদরে পরে থাকা একটি ফুল 🥺<br>
+    ঢাকা হলে এডাপশনে দেওয়ার চেষ্টা করতাম॥ কিন্তু লোকেশন মাদারীপুর…
+    `,
+    post_attachment: `./assets/images/post_image/rifat.png`,
+
+    likes: "70",
+    comments: "40",
+    author: {
+      name: `Sonia Refat`,
+      author_url: `https://web.facebook.com/soniareefat`,
+      author_image:`./assets/images/author/rifat.jpg`,
+
+    },
+  },
+  {
+    id: 7,
+    post_publish_date: `7h`,
+    post_content: `
+    Never compare yourself with anyone, the creator created you with your own beauty-😊 <br>
+    →Eid Mubarak Everyone 💙    
+    `,
+    post_attachment: `./assets/images/post_image/sahin.jpg`,
+
+    likes: "220",
+    comments: "56",
+    author: {
+      name: `Shahin Bin Salim`,
+      author_url: `https://web.facebook.com/profile.php?id=100047389165360`,
+      author_image:`./assets/images/author/sahin.jpg`,
+
+    },
+  },
+  {
+    id: 8,
+    post_publish_date: `4m`,
+    post_content: `বৃষ্টির সময়, বারান্দার সামনে...
+
+    `,
+    post_attachment: `./assets/images/post_image/junu.jpg`,
+
+    likes: "26",
+    comments: "4",
+    author: {
+      name: `Junaid Aman Junu`,
+      author_url: `https://web.facebook.com/junaid.amc`,
+      author_image:`./assets/images/author/junu.jpg`,
+
+    },
+  },
+];
